cfb84494f50c3f6f0b960c45f6e97f52d92dcfea
Pham Bao Duy <56706918+pbduy@users.noreply.github.com>
2020-07-10 17:00:36 +0700

    Animation

---
+++ Game2D/Assets/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public Vector2 newPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPos = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().position = newPos;
+    }
+}
