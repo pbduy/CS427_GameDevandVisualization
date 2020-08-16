1ab66a4e9df99097dd830171602756e563f1602a
Pham Bao Duy <56706918+pbduy@users.noreply.github.com>
2020-08-16 17:23:39 +0700

    Update folder Midterm requirements

---
+++ 1751063/Project/FlappyBirdie/Assets/Scripts/Column.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour
+{
+    public AudioClip scoreClip;
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.GetComponent<Birdie> () != null){
+            Birdie controller = other.GetComponent<Birdie>();
+            GameControl.instance.BirdieScored ();
+            controller.PlaySound(scoreClip);
+        }
+            
+    }
+}
